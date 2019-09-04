00c204ac6293f92bd8c67a5372880a8baf32e5dc
Joel R. Paderagao <joel.paderagao@outlook.com>
2019-09-04 20:45:26 +0800

    migrate

---
+++ PMI.DevOps.Monitoring/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PMI.DevOps.Monitoring.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
